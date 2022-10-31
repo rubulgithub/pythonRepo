d7098caa248d251275234e0791b415e7f48fcb45
rubulgithub <rubulislam005@gmail.com>
2022-10-31 10:42:09 +0530

    first

--- fib.py
+++ fib.py
@@ -1,1 +1,18 @@
-print("hello")
+
+
+def fib(n):
+
+    a=0
+    b=1
+
+    print(a)
+    print(b)
+
+    for i in range(2,n):
+
+        c=a+b
+        a=b
+        b=c
+        print(a+b)
+
+fib(10)
