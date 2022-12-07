af8ed3b656a4f04647340b5f896fc50195e0fa86
rubulgithub <rubulislam005@gmail.com>
2022-12-07 18:37:46 +0530

    first

--- Guessing_game.py
+++ Guessing_game.py
@@ -1,7 +1,7 @@
-n=18
+
 number_of_guesses=1
-print("Number of Guesses is Limitted to just 9")
-while(number_of_guesses<=9):
+print("Number of Guesses is Limited to just 9")
+while number_of_guesses<=9:
     guess_number=int(input("Enter the number\n"))
     if guess_number<18:
         print("Enter a Number Greater than this")
@@ -10,8 +10,10 @@ while(number_of_guesses<=9):
     else:
         print("You Won\n")
         print("Congrats\n")
-        number_of_guesses=number_of_guesses+1
         print("You took", number_of_guesses, "Guess to finish the game")
+        break
+    print(9-number_of_guesses,"Number of Guesses Left")
+    number_of_guesses=number_of_guesses+1
 if number_of_guesses>9:
     print("Game Over")
     print("Thank you for playing")
